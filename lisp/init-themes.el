ccc8b0c6d90c4729b0b22b75ca6ca17abed154d8
dorant <bjorn.d.svensson@gmail.com>
2017-07-27 08:46:57 +0200

    Improved powerline: own slim theme with linenumber to left
    Change-Id: Ic3085434afb70907c44b98c5be42e0619140f53f

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,16 +1,78 @@
 (load-theme 'solarized t)  ; Load without confirm
 
-;; Powerline - nicer status line
-;; Issues? Look at:
-;;         http://irrationalrose.com/2015/05/29/workaround-for-srgb-colour-issue-for-powerline-on-os-x.html
-(add-to-list 'load-path (expand-file-name "lisp/powerline" user-emacs-directory))
-(require 'powerline)
-(powerline-default-theme)
-(setq powerline-default-separator 'slant)
-
 ;; Solarized dark theme
 (custom-set-variables
  '(frame-background-mode (quote dark)))
 (enable-theme 'solarized)
+
+
+;; Powerline - nicer status line
+;; Issues? Look at:
+;;         http://irrationalrose.com/2015/05/29/workaround-for-srgb-colour-issue-for-powerline-on-os-x.html
+;;         or check variable in powerline
+;;
+;; Updated with
+;; https://github.com/milkypostman/powerline/issues/101
+(add-to-list 'load-path (expand-file-name "lisp/powerline" user-emacs-directory))
+(require 'powerline)
+(setq powerline-default-separator 'slant)
+(setq powerline-display-buffer-size nil)
+(setq powerline-display-mule-info nil)
+;;(powerline-default-theme)
+
+(defun powerline-default-linenum-theme ()
+  "Setup the default mode-line with line number lhs."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" nil 'l)
+                                     (powerline-raw "%4l" nil 'l)
+                                     (powerline-raw ":" nil 'l)
+                                     (powerline-raw "%3c" nil 'r)
+                                     (when powerline-display-buffer-size
+                                       (powerline-buffer-size nil 'l))
+                                     (when powerline-display-mule-info
+                                       (powerline-raw mode-line-mule-info nil 'l))
+                                     (powerline-buffer-id nil 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " ")
+                                     (funcall separator-left mode-line face1)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) face2 'l))))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (unless window-system
+                                       (powerline-raw (char-to-string #xe0a1) face1 'l))
+                                     (funcall separator-right face1 mode-line)
+                                     (powerline-raw " ")
+                                     (powerline-raw "%6p" nil 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face2 face1)))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
+(powerline-default-linenum-theme)
 
 (provide 'init-themes)
