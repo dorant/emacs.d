524c8d73e77e2706958a9ee88fa3a17056016db8
Björn Svensson A <bjorn.d.svensson@gmail.com>
2018-08-28 13:04:40 +0200

    Adding rename and move file/buffer functions

--- init.el
+++ init.el
@@ -641,7 +641,7 @@ inserted between the braces between the braces."
 ; --------------------------------------------------------
 ; What to run when make
 ; --------------------------------------------------------
-(defvar make-program "gmake -f ~/Makefile"
+(defvar make-program "make -f ~/Makefile"
    "Default program to run at `make'.")
 
 ; --------------------------------------------------------
@@ -826,6 +826,37 @@ inserted between the braces between the braces."
   (if (and buffer-dir current-prefix-arg)
       (shell-remote-cd buffer-dir))
 )
+
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+	  (message "A buffer named '%s' already exists!" new-name)
+	(progn (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil))))))
+
+(defun move-buffer-file (dir)
+  "Moves both current buffer and file it's visiting to DIR."
+  (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+	  (if (string-match dir "\\(?:/\\|\\\\)$")
+	      (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (progn (copy-file filename newname 1)
+             (delete-file filename)
+             (set-visited-file-name newname)
+             (set-buffer-modified-p nil)
+             t))))
 
 ;;----------------------------------------------------------------------------
 ;; Key setup
